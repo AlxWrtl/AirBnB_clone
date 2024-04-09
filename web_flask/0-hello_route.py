7e45d43a21258fed82ff7377be8911f5631a992a
AlxWrtl <indexes-benzine0p@icloud.com>
2024-04-09 13:48:49 +0200

    add a new line

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -19,6 +19,7 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
     """Run the Flask web application on host 0.0.0.0 and port 5000."""
     app.run(host='0.0.0.0', port=5000)
