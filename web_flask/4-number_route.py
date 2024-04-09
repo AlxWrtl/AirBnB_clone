fc8b426a2f90cc7d6549176ff5533c67e7db3639
AlxWrtl <indexes-benzine0p@icloud.com>
2024-04-09 16:28:16 +0200

    changed the port number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -79,4 +79,4 @@ def number_to_number(n):
 
 if __name__ == "__main__":
     """Run the Flask web application on host 0.0.0.0 and port 5000."""
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
