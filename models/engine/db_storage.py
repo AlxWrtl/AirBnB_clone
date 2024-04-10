bac968d0a04f5eb555a6338d8b24728909e21230
AlxWrtl <indexes-benzine0p@icloud.com>
2024-04-10 15:46:13 +0200

    add the method reload

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -91,4 +91,5 @@ class DBStorage:
 
     def close(self):
         """call remove method on the private session"""
-        self.__session.remove()
+        self.reload()
+        self.__session.close()
